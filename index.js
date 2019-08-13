67a39e6ca0162e42079ba5876aeb6d58d0aabe7f
Mary Farner <maryfarner@gmail.com>
2019-08-13 23:57:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,40 @@ function theBeatlesPlay(musicians, instruments) {
 
 }
 
+//Adds "!!!" to the end of each fact
+function johnLennonFacts(facts) {
+
+  var i=0
+  
+  while(i < facts.length()) {
+    facts[i] = facts[i] + "!!!"
+    
+  }
+  
+  return facts
+
+}
+
+//concetenates the musician and the instrument and stores
+//in new array
+function iLoveTheBeatles(num) {
+  var phrases = []
+  var diff = 15 - num
+  
+  if(diff < 0) {
+    return phrases
+  }
+  
+  var i = 0
+  do {
+    
+    phrases[i] = "I love the Beatles!"
+    i++
+    
+  } while(i<diff)
+  
+  return phrases
+}
+
+
 
